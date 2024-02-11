2a82577ffda40ffe44642e768e68c1a5a8f89a32
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-11 00:09:05 -0500

    Add: LSP keymaps

--- config/lsp.nix
+++ config/lsp.nix
@@ -17,5 +17,18 @@
         enable = true;
       };
     };
+    keymaps = {
+      diagnostic = {
+        "]d" = "goto_next";
+        "[d" = "goto_prev";
+      };
+      lspBuf = {
+        K = "hover";
+        gD = "references";
+        gd = "definition";
+        gi = "implementation";
+        gt = "type_definition";
+      };
+    };
   };
 }
