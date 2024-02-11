460ca88fb5642b615279282e1ca4d1698b5fd096
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-10 22:37:23 -0500

    Add: Config bufferline

--- config/bufferline.nix
+++ config/bufferline.nix
@@ -1,5 +1,11 @@
 {
   plugins.bufferline = {
     enable = true;
+    colorIcons = true;
+    diagnostics = "nvim_lsp";
+    mode = "buffers"; # "buffers" or "tabs"
+    separatorStyle = "thick"; # "slant" or "slope" or "thick" or "thin" or "none"
+    showBufferCloseIcons = false;
+    showTabIndicators = true;
   };
 }
--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,7 @@
   # Import all your configuration modules here
   imports = [
     ./bufferline.nix
+    ./lualine.nix
     ./themes.nix
   ];
 
