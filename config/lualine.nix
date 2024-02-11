b3dee88570f82d67fc0767b8cbccb47b9a7d34c6
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-10 22:47:33 -0500

    Add: Config for lualine

---
+++ config/lualine.nix
@@ -0,0 +1,14 @@
+{
+  plugins.lualine = {
+    enable = true;
+    iconsEnabled = true;
+    componentSeparators = {
+      left = "";
+      right = "";
+    };
+    sectionSeparators = {
+      left = "";
+      right = "";
+    };
+  };
+}
