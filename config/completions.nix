037bb3378f925784c026995a8fb82774ef1a1e82
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-11 00:33:15 -0500

    Add: Mapping for nvim-cmp completion selection

--- config/completions.nix
+++ config/completions.nix
@@ -8,5 +8,26 @@
       { name = "path"; }
       { name = "luasnip"; }
     ];
+    mapping = {
+      "<C-Space>" = "cmp.mapping.complete()";
+      "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+      "<C-e>" = "cmp.mapping.close()";
+      "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      "<CR>" = "cmp.mapping.confirm({ select = true })";
+      "<C-p>" = {
+        action = "cmp.mapping.select_prev_item()";
+        modes = [
+          "i"
+          "s"
+        ];
+      };
+      "<C-n>" = {
+        action = "cmp.mapping.select_next_item()";
+        modes = [
+          "i"
+          "s"
+        ];
+      };
+    };
   };
 }
