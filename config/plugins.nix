c19835daea71d898676b4788f983fdc639872861
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-16 01:06:20 -0500

    Add: specs.nvim plugin

--- config/plugins.nix
+++ config/plugins.nix
@@ -107,6 +107,16 @@ in
       theme = "dark";
     };
     persistence.enable = true;
+    specs = {
+      enable = true;
+      min_jump = 10;
+      increment = 10;
+      delay = 10;
+      width = 20;
+      color = "#94e2d5";
+      fader.builtin = "pulse_fader";
+      resizer.builtin = "shrink_resizer";
+    };
   };
 
   extraPackages = with pkgs; [
