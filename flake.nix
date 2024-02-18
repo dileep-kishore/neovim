a8032feec377553c4a9189c8e57663e0f2e47389
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-18 11:03:33 -0500

    Chore: Format flake.nix with alejandra

--- flake.nix
+++ flake.nix
@@ -7,49 +7,53 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = { self, nixpkgs, nixvim, flake-parts, ... }@inputs:
-    let
-      config = import ./config; # import the module directly
-      # NOTE: Function to create a customized pkgs with `allowUnfree` set to true
-      mkPkgs = system:
-        import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true; # Enable unfree packages
-            # You can set other Nixpkgs config options here as needed
+  outputs = {
+    self,
+    nixpkgs,
+    nixvim,
+    flake-parts,
+    ...
+  } @ inputs: let
+    config = import ./config; # import the module directly
+    # NOTE: Function to create a customized pkgs with `allowUnfree` set to true
+    mkPkgs = system:
+      import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true; # Enable unfree packages
+          # You can set other Nixpkgs config options here as needed
+        };
+      };
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+
+      perSystem = {system, ...}: let
+        pkgs =
+          mkPkgs system; # NOTE: Use the custom pkgs with allowUnfree enabled
+        nixvimLib = nixvim.lib.${system};
+        nixvim' = nixvim.legacyPackages.${system};
+        nvim = nixvim'.makeNixvimWithModule {
+          inherit pkgs;
+          module = config;
+          # You can use `extraSpecialArgs` to pass additional arguments to your module files
+          extraSpecialArgs = {
+            # inherit (inputs) foo;
           };
         };
-    in flake-parts.lib.mkFlake { inherit inputs; } {
-      systems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-
-      perSystem = { system, ... }:
-        let
-          pkgs =
-            mkPkgs system; # NOTE: Use the custom pkgs with allowUnfree enabled
-          nixvimLib = nixvim.lib.${system};
-          nixvim' = nixvim.legacyPackages.${system};
-          nvim = nixvim'.makeNixvimWithModule {
-            inherit pkgs;
-            module = config;
-            # You can use `extraSpecialArgs` to pass additional arguments to your module files
-            extraSpecialArgs = {
-              # inherit (inputs) foo;
-            };
-          };
-        in {
-          checks = {
-            # Run `nix flake check .` to verify that your config is not broken
-            default = nixvimLib.check.mkTestDerivationFromNvim {
-              inherit nvim;
-              name = "My personal neovim configuration";
-            };
-          };
-
-          packages = {
-            # Lets you run `nix run .` to start nixvim
-            default = nvim;
+      in {
+        checks = {
+          # Run `nix flake check .` to verify that your config is not broken
+          default = nixvimLib.check.mkTestDerivationFromNvim {
+            inherit nvim;
+            name = "My personal neovim configuration";
           };
         };
+
+        packages = {
+          # Lets you run `nix run .` to start nixvim
+          default = nvim;
+        };
+      };
     };
 }
