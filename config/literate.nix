5ef82d88bdc2e941c39eb51e900b05f9a2f7945c
Dileep Kishore <k.dileep1994@gmail.com>
2024-02-18 17:21:41 -0500

    Change: Disable markdown image rendering by default

--- config/literate.nix
+++ config/literate.nix
@@ -1,7 +1,7 @@
 {
   plugins.molten = {
     enable = true;
-    autoOpenOutput = true;
+    autoOpenOutput = false;
     copyOutput = false;
     imageProvider = "image.nvim";
     outputVirtLines = true;
@@ -10,5 +10,25 @@
   };
   plugins.image = {
     enable = true;
+    editorOnlyRenderWhenFocused = true;
+    maxHeightWindowPercentage = 50;
+    maxWidthWindowPercentage = 50;
+    integrations = {
+      markdown = {
+        enabled = true;
+        clearInInsertMode = true;
+        onlyRenderImageAtCursor = true;
+      };
+      neorg = {
+        enabled = true;
+        clearInInsertMode = true;
+        onlyRenderImageAtCursor = true;
+      };
+      syslang = {
+        enabled = true;
+        clearInInsertMode = true;
+        onlyRenderImageAtCursor = true;
+      };
+    };
   };
 }
