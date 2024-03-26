79bf9e38dd7dc4161486942af8b3e2d1d067d700
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-26 19:01:25 -0400

    feat: use ft_color for incline icon

--- config/incline.nix
+++ config/incline.nix
@@ -56,7 +56,7 @@
         local ft_icon, ft_color = devicons.get_icon_color(filename)
         local modified = vim.bo[props.buf].modified
         local res = {
-          ft_icon and { " ", ft_icon, " ", guibg = darkenHexColor(ft_color, 0.2), guifg = helpers.contrast_color(ft_color) } or "",
+          ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
           ' ',
           { filename, gui = modified and 'bold,italic' or 'bold' },
           guibg = colors.base,
