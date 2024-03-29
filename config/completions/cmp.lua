b8c9d97deea500dea0076a067d87f551abb17ef8
Dileep Kishore <k.dileep1994@gmail.com>
2024-03-29 00:30:49 -0400

    refactor: move completions extraConfigLua to lua file

---
+++ config/completions/cmp.lua
@@ -0,0 +1,21 @@
+local cmp = require 'cmp'
+cmp.setup.cmdline('/', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' },
+  },
+})
+
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' },
+      },
+    },
+  }),
+})
--- config/completions/completions.nix
+++ config/completions/completions.nix
@@ -39,40 +39,7 @@
         };
       };
       formatting.fields = ["kind" "abbr" "menu"];
-      formatting.format = ''
-        function(entry, vim_item)
-          local colors = {
-              _nc = "#16141f",
-              base = "#191724",
-              surface = "#1f1d2e",
-              overlay = "#26233a",
-              muted = "#6e6a86",
-              subtle = "#908caa",
-              text = "#e0def4",
-              love = "#eb6f92",
-              gold = "#f6c177",
-              rose = "#ebbcba",
-              pine = "#31748f",
-              foam = "#9ccfd8",
-              iris = "#c4a7e7",
-              highlight_low = "#21202e",
-              highlight_med = "#403d52",
-              highlight_high = "#524f67",
-              none = "NONE",
-          }
-          local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50, symbol_map = { Copilot = " " } })(entry, vim_item)
-          local strings = vim.split(kind.kind, "%s", { trimempty = true })
-          kind.kind = " " .. (strings[1] or "") .. " "
-          local menu_item = ""
-          if strings[2] == "" then
-            menu_item = "Copilot"
-          else
-            menu_item = strings[2]
-          end
-          kind.menu = "    (" .. menu_item .. ")"
-          return kind
-        end
-      '';
+      formatting.format = builtins.readFile ./cmp_format.lua;
       sources = [
         {name = "nvim_lsp";}
         {name = "luasnip";}
@@ -133,27 +100,5 @@
     };
   };
 
-  extraConfigLua = ''
-    local cmp = require('cmp')
-    cmp.setup.cmdline('/', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    })
-
-    cmp.setup.cmdline(':', {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = 'path' }
-      }, {
-        {
-          name = 'cmdline',
-          option = {
-            ignore_cmds = { 'Man', '!' }
-          }
-        }
-      })
-    })
-  '';
+  extraConfigLua = builtins.readFile ./cmp.lua;
 }
