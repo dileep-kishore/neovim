d7bb60dc7ae7982e38a0b15f1448427f6710d0b4
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-21 11:40:29 -0400

    fix(catppuccin): breaking changes after update

--- config/themes/catppuccin.nix
+++ config/themes/catppuccin.nix
@@ -1,52 +1,54 @@
 {
   colorschemes.catppuccin = {
     enable = false;
-    flavour = "mocha";
-    disableBold = false;
-    disableItalic = false;
-    background.dark = "mocha";
-    background.light = "latte";
-    styles = {
-      functions = ["italic"];
-      types = ["italic"];
-    };
-    integrations = {
-      alpha = true;
-      dap = {
-        enable_ui = true;
-        enabled = true;
+    settings = {
+      flavour = "mocha";
+      disableBold = false;
+      disableItalic = false;
+      background.dark = "mocha";
+      background.light = "latte";
+      styles = {
+        functions = ["italic"];
+        types = ["italic"];
       };
-      cmp = true;
-      leap = true;
-      gitsigns = true;
-      harpoon = true;
-      headlines = true;
-      illuminate = {
-        enabled = true;
-        lsp = true;
+      integrations = {
+        alpha = true;
+        dap = {
+          enable_ui = true;
+          enabled = true;
+        };
+        cmp = true;
+        leap = true;
+        gitsigns = true;
+        harpoon = true;
+        headlines = true;
+        illuminate = {
+          enabled = true;
+          lsp = true;
+        };
+        lsp_saga = true;
+        lsp_trouble = true;
+        markdown = true;
+        mason = true;
+        mini = {enabled = true;};
+        native_lsp = {
+          enabled = true;
+          inlay_hints.background = true;
+        };
+        navic.enabled = true;
+        neogit = true;
+        neotest = true;
+        noice = true;
+        notify = true;
+        nvimtree = true;
+        octo = true;
+        ufo = true;
+        overseer = true;
+        rainbow_delimiters = true;
+        telescope = {enabled = true;};
+        symbols_outline = true;
+        which_key = true;
       };
-      lsp_saga = true;
-      lsp_trouble = true;
-      markdown = true;
-      mason = true;
-      mini = {enabled = true;};
-      native_lsp = {
-        enabled = true;
-        inlay_hints.background = true;
-      };
-      navic.enabled = true;
-      neogit = true;
-      neotest = true;
-      noice = true;
-      notify = true;
-      nvimtree = true;
-      octo = true;
-      ufo = true;
-      overseer = true;
-      rainbow_delimiters = true;
-      telescope = {enabled = true;};
-      symbols_outline = true;
-      which_key = true;
     };
   };
 }
