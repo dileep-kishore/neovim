ab2c17b883b4495a544535538c5637699d47a1cf
Dileep Kishore <k.dileep1994@gmail.com>
2024-04-21 11:36:39 -0400

    fix(rose-pine): breaking changes after update

--- config/themes/rose-pine.nix
+++ config/themes/rose-pine.nix
@@ -1,110 +1,113 @@
 {
   colorschemes.rose-pine = {
     enable = true;
-    style = "main";
-    boldVerticalSplit = false;
-    dimInactive = false;
-    highlightGroups = {
-      "@type" = {bold = true;};
-      "@variable" = {italic = false;};
-      "@keyword" = {italic = true;};
-      "@keyword.conditional" = {italic = true;};
-      "@keyword.repeat" = {italic = true;};
-      "@function" = {italic = true;};
-      "@function.method.call" = {italic = true;};
-      "LeapBackdrop" = {link = "Comment";};
-      "@markup.raw" = {
-        fg = "gold";
-        bg = "surface";
+    settings = {
+      variant = "main";
+      dark_variant = "main";
+      dim_inactive_windows = false;
+      extend_background_behind_borders = false;
+      highlight_groups = {
+        "@type" = {bold = true;};
+        "@variable" = {italic = false;};
+        "@keyword" = {italic = true;};
+        "@keyword.conditional" = {italic = true;};
+        "@keyword.repeat" = {italic = true;};
+        "@function" = {italic = true;};
+        "@function.method.call" = {italic = true;};
+        "LeapBackdrop" = {link = "Comment";};
+        "@markup.raw" = {
+          fg = "gold";
+          bg = "surface";
+        };
+        MiniIndentscopeSymbol = {fg = "foam";};
+        MiniIndentscopeSymbolOff = {fg = "foam";};
+        EndOfBuffer = {
+          fg = "base";
+          bg = "base";
+        };
+        NotifyERRORBorder = {
+          bg = "base";
+        };
+        NotifyWARNBorder = {
+          bg = "base";
+        };
+        NotifyINFOBorder = {
+          bg = "base";
+        };
+        NotifyDEBUGBorder = {
+          bg = "base";
+        };
+        NotifyTRACEBorder = {
+          bg = "base";
+        };
+        NormalFloat = {
+          bg = "base";
+        };
+        Float = {
+          bg = "base";
+        };
+        Pmenu = {
+          bg = "base";
+        };
+        FloatBorder = {
+          fg = "highlight_high";
+          bg = "base";
+        };
+        CmpItemMenu = {
+          fg = "rose";
+          bg = "NONE";
+          italic = true;
+        };
+        CmpItemKindCopilot = {
+          fg = "pine";
+        };
+        TelescopeBorder = {
+          fg = "overlay";
+          bg = "overlay";
+        };
+        TelescopeNormal = {
+          fg = "subtle";
+          bg = "overlay";
+        };
+        TelescopeSelection = {
+          fg = "text";
+          bg = "highlight_med";
+        };
+        TelescopeSelectionCaret = {
+          fg = "love";
+          bg = "highlight_med";
+        };
+        TelescopeMultiSelection = {
+          fg = "text";
+          bg = "highlight_high";
+        };
+        TelescopeTitle = {
+          fg = "base";
+          bg = "love";
+        };
+        TelescopePromptTitle = {
+          fg = "base";
+          bg = "pine";
+        };
+        TelescopePreviewTitle = {
+          fg = "base";
+          bg = "iris";
+        };
+        TelescopePromptNormal = {
+          fg = "text";
+          bg = "surface";
+        };
+        TelescopePromptBorder = {
+          fg = "surface";
+          bg = "surface";
+        };
+        TreeSitterContext = {bg = "highlight_low";};
+        Headline1 = {bg = "#48454b";}; # iris
+        Headline2 = {bg = "#434849";}; # foam
+        Headline3 = {bg = "#4c4747";}; # rose
+        Headline4 = {bg = "#4e4942";}; # gold
+        Headline5 = {bg = "#323b3e";}; # pine
       };
-      MiniIndentscopeSymbol = {fg = "foam";};
-      MiniIndentscopeSymbolOff = {fg = "foam";};
-      EndOfBuffer = {
-        fg = "base";
-        bg = "base";
-      };
-      NotifyERRORBorder = {
-        bg = "base";
-      };
-      NotifyWARNBorder = {
-        bg = "base";
-      };
-      NotifyINFOBorder = {
-        bg = "base";
-      };
-      NotifyDEBUGBorder = {
-        bg = "base";
-      };
-      NotifyTRACEBorder = {
-        bg = "base";
-      };
-      NormalFloat = {
-        bg = "base";
-      };
-      Float = {
-        bg = "base";
-      };
-      Pmenu = {
-        bg = "base";
-      };
-      FloatBorder = {
-        fg = "highlight_high";
-        bg = "base";
-      };
-      CmpItemMenu = {
-        fg = "rose";
-        bg = "NONE";
-        italic = true;
-      };
-      CmpItemKindCopilot = {
-        fg = "pine";
-      };
-      TelescopeBorder = {
-        fg = "overlay";
-        bg = "overlay";
-      };
-      TelescopeNormal = {
-        fg = "subtle";
-        bg = "overlay";
-      };
-      TelescopeSelection = {
-        fg = "text";
-        bg = "highlight_med";
-      };
-      TelescopeSelectionCaret = {
-        fg = "love";
-        bg = "highlight_med";
-      };
-      TelescopeMultiSelection = {
-        fg = "text";
-        bg = "highlight_high";
-      };
-      TelescopeTitle = {
-        fg = "base";
-        bg = "love";
-      };
-      TelescopePromptTitle = {
-        fg = "base";
-        bg = "pine";
-      };
-      TelescopePreviewTitle = {
-        fg = "base";
-        bg = "iris";
-      };
-      TelescopePromptNormal = {
-        fg = "text";
-        bg = "surface";
-      };
-      TelescopePromptBorder = {
-        fg = "surface";
-        bg = "surface";
-      };
-      TreeSitterContext = {bg = "highlight_low";};
-      Headline1 = {bg = "#48454b";}; # iris
-      Headline2 = {bg = "#434849";}; # foam
-      Headline3 = {bg = "#4c4747";}; # rose
-      Headline4 = {bg = "#4e4942";}; # gold
-      Headline5 = {bg = "#323b3e";}; # pine
     };
   };
 }
