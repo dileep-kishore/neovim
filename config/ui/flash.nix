5177b5c073f4aaa763bef916f816dd1abd031847
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-05 17:16:09 -0400

    fix(flash): update deprecated options

--- config/ui/flash.nix
+++ config/ui/flash.nix
@@ -1,18 +1,20 @@
 {
   plugins.flash = {
     enable = true;
-    continue = false;
-    modes.search.enabled = false;
-    jump = {
-      autojump = false;
-      history = false;
-      jumplist = true;
-      nohlsearch = true;
-    };
-    label = {
-      after = true;
-      minPatternLength = 0;
-      rainbow.enabled = false;
+    settings = {
+      continue = false;
+      modes.search.enabled = false;
+      jump = {
+        autojump = false;
+        history = false;
+        jumplist = true;
+        nohlsearch = true;
+      };
+      label = {
+        after = true;
+        minPatternLength = 0;
+        rainbow.enabled = false;
+      };
     };
   };
 }
