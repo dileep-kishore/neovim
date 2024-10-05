2c775af6dcc671ace360b6d550dc51d84c4854c3
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-05 17:18:22 -0400

    fix(whichkey): update deprecated options

--- config/keymaps/whichkey.nix
+++ config/keymaps/whichkey.nix
@@ -1,42 +1,44 @@
 {
   plugins.which-key = {
     enable = true;
-    layout = {align = "center";};
-    window = {
-      border = "rounded";
-      position = "bottom";
-      winblend = 10;
-      margin = {
-        top = 1;
-        right = 1;
-        bottom = 2;
-        left = 1;
+    settings = {
+      layout = {align = "center";};
+      window = {
+        border = "rounded";
+        position = "bottom";
+        winblend = 10;
+        margin = {
+          top = 1;
+          right = 1;
+          bottom = 2;
+          left = 1;
+        };
       };
-    };
-    registrations = {
-      "<Leader>q" = "+Close";
-      "<Leader>a" = "+Alpha dashboard";
-      "<leader>g" = "+Git";
-      "<leader>h" = "+Git hunk";
-      "<leader>[" = "+prev";
-      "<leader>]" = "+next";
-      "<leader>f" = "+File search";
-      "<leader>s" = "+Text search";
-      "<leader>x" = "+Trouble";
-      "<leader>l" = "+LSP";
-      "<leader>d" = "+DAP";
-      "<leader>t" = "+Telescope";
-      "<leader>T" = "+ToggleTerm";
-      "<leader>r" = "+Refactor";
-      "<leader>z" = "+Zen";
-      "<leader>R" = "+Auto-session";
-      "<leader>c" = "+ChatGPT";
-      "<leader>n" = "+New file";
-      "<leader>m" = "+Grapple";
-      "<leader>i" = "+Iron";
-      "<leader>p" = "+Parameter swap";
-      "<leader>cd" = "Diagnostics gpt";
-      "<leader>cs" = "Diagnostics search";
+      registrations = {
+        "<Leader>q" = "+Close";
+        "<Leader>a" = "+Alpha dashboard";
+        "<leader>g" = "+Git";
+        "<leader>h" = "+Git hunk";
+        "<leader>[" = "+prev";
+        "<leader>]" = "+next";
+        "<leader>f" = "+File search";
+        "<leader>s" = "+Text search";
+        "<leader>x" = "+Trouble";
+        "<leader>l" = "+LSP";
+        "<leader>d" = "+DAP";
+        "<leader>t" = "+Telescope";
+        "<leader>T" = "+ToggleTerm";
+        "<leader>r" = "+Refactor";
+        "<leader>z" = "+Zen";
+        "<leader>R" = "+Auto-session";
+        "<leader>c" = "+ChatGPT";
+        "<leader>n" = "+New file";
+        "<leader>m" = "+Grapple";
+        "<leader>i" = "+Iron";
+        "<leader>p" = "+Parameter swap";
+        "<leader>cd" = "Diagnostics gpt";
+        "<leader>cs" = "Diagnostics search";
+      };
     };
   };
 }
