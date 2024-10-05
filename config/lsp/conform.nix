3f2a180d104bacdf4796312927dc3c26943d92a6
Dileep Kishore <k.dileep1994@gmail.com>
2024-10-05 17:19:43 -0400

    fix(lsp): update conform deprecated options

--- config/lsp/conform.nix
+++ config/lsp/conform.nix
@@ -1,33 +1,35 @@
 {
   plugins.conform-nvim = {
     enable = true;
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 500;
-    };
-    # Map of filetype to formatters
-    formattersByFt = {
-      lua = [ "stylua" ];
-      nix = [ "alejandra" ];
-      sh = [ "shfmt" ];
-      go = [ "gofmt" ];
-      rust = [ "rustfmt" ];
-      python = [ "isort" "ruff_format" ];
-      javascript = [ "prettier" ];
-      javascriptreact = [ "prettier" ];
-      typescript = [ "prettier" ];
-      typescriptreact = [ "prettier" ];
-      svelte = [ "prettier" ];
-      css = [ "prettier" ];
-      html = [ "prettier" ];
-      json = [ "prettier" ];
-      yaml = [ "prettier" ];
-      markdown = [ "prettier" ];
-      typst = [ "typstfmt" ];
-      # Use the "_" filetype to run formatters on filetypes that don't have other formatters configured.
-      "_" = [ "trim_newlines" "trim_whitespace" ];
-      # # Use the "*" filetype to run formatters on all filetypes.
-      # "*" = [ "codespell" ];
+    settings = {
+      formatOnSave = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      # Map of filetype to formatters
+      formattersByFt = {
+        lua = ["stylua"];
+        nix = ["alejandra"];
+        sh = ["shfmt"];
+        go = ["gofmt"];
+        rust = ["rustfmt"];
+        python = ["isort" "ruff_format"];
+        javascript = ["prettier"];
+        javascriptreact = ["prettier"];
+        typescript = ["prettier"];
+        typescriptreact = ["prettier"];
+        svelte = ["prettier"];
+        css = ["prettier"];
+        html = ["prettier"];
+        json = ["prettier"];
+        yaml = ["prettier"];
+        markdown = ["prettier"];
+        typst = ["typstfmt"];
+        # Use the "_" filetype to run formatters on filetypes that don't have other formatters configured.
+        "_" = ["trim_newlines" "trim_whitespace"];
+        # # Use the "*" filetype to run formatters on all filetypes.
+        # "*" = [ "codespell" ];
+      };
     };
   };
 }
