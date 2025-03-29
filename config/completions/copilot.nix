8d31884d16e2cfeb17f854b117d7465414985a62
Dileep Kishore <k.dileep1994@gmail.com>
2025-03-29 16:10:54 -0400

    feat(copilot): use custom copilot pkg to fix server issues

--- config/completions/copilot.nix
+++ config/completions/copilot.nix
@@ -1,6 +1,24 @@
-{
+{pkgs, ...}: let
+  # TODO: Remove this once the nix pkg is updated
+  copilot-lua-custom = pkgs.vimUtils.buildVimPlugin {
+    pname = "copilot.lua";
+    version = "2025-03-29";
+    src = pkgs.fetchFromGitHub {
+      owner = "zbirenbaum";
+      repo = "copilot.lua";
+      rev = "0d56d43d2d2c0c8cf897a338be402bd639a906ae";
+      hash = "sha256-OGursz/xVjG5xBrgQCqnazOpH39uojpbgJ9Tvhqpji8=";
+    };
+    meta.homepage = "https://github.com/zbirenbaum/copilot.lua";
+  };
+in {
+  extraPlugins = [
+    copilot-lua-custom
+  ];
   plugins.copilot-lua = {
-    enable = false;
+    enable = true;
+    package = copilot-lua-custom;
+    nodePackage = pkgs.nodejs_23;
     settings = {
       panel = {
         enabled = false;
