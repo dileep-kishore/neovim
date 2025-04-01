cf2c2f9835249abcb4b56b52d517f062c45d682b
Dileep Kishore <k.dileep1994@gmail.com>
2025-03-31 21:31:27 -0400

    feat(treesitter): enable incremental selection

--- config/treesitter/treesitter.nix
+++ config/treesitter/treesitter.nix
@@ -3,6 +3,15 @@
     enable = true;
     settings = {
       highlight.enable = true;
+      incremental_selection = {
+        enable = true;
+        keymaps = {
+          init_selection = "<CR>";
+          node_incremental = "<CR>";
+          scope_incremental = false;
+          node_decremental = "<Backspace>";
+        };
+      };
     };
   };
 }
