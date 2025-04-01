fc2361399fd8a88ef91fe42e3dd42d772808e0f7
Dileep Kishore <k.dileep1994@gmail.com>
2025-04-01 17:46:17 -0400

    feat(matchup): disable vim-matchup
    This doesn't work properly most of the time

--- config/ui/matchup.nix
+++ config/ui/matchup.nix
@@ -1,11 +1,11 @@
 {
   plugins.vim-matchup = {
-    enable = true;
+    enable = false;
     settings = {
       surround_enabled = 1;
       transmute_enabled = 1;
       treesitter.enabled = true;
     };
   };
-  extraConfigLua = builtins.readFile ./matchup.lua;
+  # extraConfigLua = builtins.readFile ./matchup.lua;
 }
