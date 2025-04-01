7e2950898b1e79ed60217122018fded31eb9fc11
Dileep Kishore <k.dileep1994@gmail.com>
2025-03-31 21:23:39 -0400

    feat(whichkey): update layout to helix

--- config/keymaps/whichkey.nix
+++ config/keymaps/whichkey.nix
@@ -2,11 +2,7 @@
   plugins.which-key = {
     enable = true;
     settings = {
-      layout = {align = "center";};
-      win = {
-        border = "rounded";
-        wo.winblend = 10;
-      };
+      preset = "helix";
     };
   };
   extraConfigLua = builtins.readFile ./whichkey.lua;
